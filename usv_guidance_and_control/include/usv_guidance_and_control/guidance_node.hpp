de157f30509c54a356d7a6ed9b973dcd60f759e8
Mael-RABOT <mael.rabot@epitech.eu>
2024-11-13 13:49:15 +0100

    evol(guidance): add message parsing
    Signed-off-by: Mael-RABOT <mael.rabot@epitech.eu>

--- usv_guidance_and_control/include/usv_guidance_and_control/guidance_node.hpp
+++ usv_guidance_and_control/include/usv_guidance_and_control/guidance_node.hpp
@@ -1,20 +1,59 @@
+/*
+ * This file is part of the MarineMindRobotics project.
+ *
+ * Copyright (C) 2024 MarineMindRobotics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #pragma once
+
+#include <queue>
+#include <tuple>
 
 #include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/float64.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "sensor_msgs/msg/nav_sat_fix.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+
+struct ImuData {
+  std_msgs::msg::Float64 roll;   // x
+  std_msgs::msg::Float64 pitch;  // y
+  std_msgs::msg::Float64 yaw;    // z
+  std_msgs::msg::Float64 scalar; // w
+};
 
 class GuidanceNode: public rclcpp::Node {
     private:
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _sub_imu;
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _sub_gps;
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _sub_waypoints;
+        rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr _sub_imu;
+        rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr _sub_gps;
+        rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr _sub_waypoints;
 
         rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr _pub_deviation_detected;
+        rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr _pub_cmd_vel;
+        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr _pub_goal_reached;
 
-        void _imuCallback(const std_msgs::msg::String::SharedPtr msg);
-        void _gpsCallback(const std_msgs::msg::String::SharedPtr msg);
-        void _waypointsCallback(const std_msgs::msg::String::SharedPtr msg);
+        std::queue<std::pair<std_msgs::msg::Float64, std_msgs::msg::Float64>> _waypoints;
+        std::pair<std_msgs::msg::Float64, std_msgs::msg::Float64> _gps_position;
+        ImuData _imu_data;
+
+        void _imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg);
+        void _gpsCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
+        void _waypointsCallback(const nav_msgs::msg::Path::SharedPtr msg);
     public:
         GuidanceNode();
         ~GuidanceNode();
--- usv_guidance_and_control/src/guidance_node.cpp
+++ usv_guidance_and_control/src/guidance_node.cpp
@@ -1,31 +1,78 @@
+/*
+ * This file is part of the MarineMindRobotics project.
+ *
+ * Copyright (C) 2024 MarineMindRobotics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "guidance_node.hpp"
 
 GuidanceNode::GuidanceNode(): Node("guidance_node") {
-    _sub_imu = this->create_subscription<std_msgs::msg::String>("/usv/hardware_interface/imu_data", 10, std::bind(&GuidanceNode::_imuCallback, this, std::placeholders::_1));
-    _sub_gps = this->create_subscription<std_msgs::msg::String>("/usv/hardware_interface/gps_fix", 10, std::bind(&GuidanceNode::_gpsCallback, this, std::placeholders::_1));
-    _sub_waypoints = this->create_subscription<std_msgs::msg::String>("/usv/guidance/waypoints", 10, std::bind(&GuidanceNode::_waypointsCallback, this, std::placeholders::_1));
+    _sub_imu = this->create_subscription<sensor_msgs::msg::Imu>(
+        "/usv/hardware_interface/imu_data",
+        10,
+        std::bind(&GuidanceNode::_imuCallback, this, std::placeholders::_1)
+    );
+    _sub_gps = this->create_subscription<sensor_msgs::msg::NavSatFix>(
+        "/usv/hardware_interface/gps_fix",
+        10,
+        std::bind(&GuidanceNode::_gpsCallback, this, std::placeholders::_1)
+    );
+    _sub_waypoints = this->create_subscription<nav_msgs::msg::Path>(
+        "/usv/guidance/waypoints",
+        10,
+        std::bind(&GuidanceNode::_waypointsCallback, this, std::placeholders::_1)
+    );
 
     _pub_deviation_detected = this->create_publisher<std_msgs::msg::Float64>("/usv/guidance/deviation_detected", 10);
+    _pub_cmd_vel = this->create_publisher<std_msgs::msg::Float64>("/usv/hardware_interface/cmd_vel", 10);
+	_pub_goal_reached = this->create_publisher<std_msgs::msg::Bool>("/usv/guidance/goal_reached", 10);
 };
 
 GuidanceNode::~GuidanceNode() {};
 
-void GuidanceNode::_imuCallback(const std_msgs::msg::String::SharedPtr msg) {
-    // TODO: Parse the message and extract the data
+void GuidanceNode::_imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg) {
+    std_msgs::msg::Float64 roll, pitch, yaw, scalar;
+    roll.data = msg->orientation.x;
+    pitch.data = msg->orientation.y;
+    yaw.data = msg->orientation.z;
+    scalar.data = msg->orientation.w;
+    _imu_data = {roll, pitch, yaw, scalar};
+}
+
+void GuidanceNode::_gpsCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg) {
+    std_msgs::msg::Float64 latitude;
+    std_msgs::msg::Float64 longitude;
+    latitude.data = msg->latitude;
+    longitude.data = msg->longitude;
+    _gps_position = std::make_pair(latitude, longitude);
 };
 
-void GuidanceNode::_gpsCallback(const std_msgs::msg::String::SharedPtr msg) {
-    // TODO: Parse the message and extract the data
+void GuidanceNode::_waypointsCallback(const nav_msgs::msg::Path::SharedPtr msg) {
+    for (const auto& pose : msg->poses) {
+        std_msgs::msg::Float64 x;
+        std_msgs::msg::Float64 y;
+        x.data = pose.pose.position.x;
+        y.data = pose.pose.position.y;
+        _waypoints.push(std::make_pair(x, y));
+    }
 };
 
-void GuidanceNode::_waypointsCallback(const std_msgs::msg::String::SharedPtr msg) {
-    // TODO: Parse the message and extract the data
-};
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<GuidanceNode>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GuidanceNode>());
+    rclcpp::shutdown();
+    return 0;
 }
